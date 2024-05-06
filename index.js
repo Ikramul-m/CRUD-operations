15090e5d82cc04b67505230b624d6fa085b7feb1
Ikramul-m <i20727172@gmail.com>
2024-05-06 21:07:12 +0530

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello World!!!")
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
+
+app.get("/", (res, req) => {
+  res.send("Hello from Node API");
+});
