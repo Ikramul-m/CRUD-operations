0ec3705c6001226746856def366dc0447c909b61
Ikramul-m <i20727172@gmail.com>
2024-05-07 10:45:09 +0530

    .

---
+++ Models/Product.js
@@ -0,0 +1,30 @@
+const { Timestamp } = require("mongodb");
+const mongoose = require("mongoose");
+
+const ProductSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: [true, "Please enter name"],
+    },
+
+    quality: {
+      type: Number,
+      require: [true, "Please enter quality"],
+      default: 0,
+    },
+
+    price: {
+      type: Number,
+      require: true,
+      default: 0,
+    },
+    image: {
+      type: String,
+      require: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
+const PORT = 3000;
+const hostName = "localhost";
 
-app.listen(3000, () => {
-  console.log("Server listening on 3000");
+app.listen(PORT, () => {
+  console.log(`Server running on http://${hostName}:${PORT}`);
 });
 
 app.get("/", (req, res) => {
   res.send("Hello from Node API");
 });
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/operations")
+  .then(() => {
+    console.log("Connected to Database...");
+  })
+  .catch(() => {
+    console.log("Connection Failed");
+  });
