863cece983d4a7547708b17bc0cc7a2af0493057
Ikramul-m <i20727172@gmail.com>
2024-05-08 14:20:23 +0530

    .

--- index.js
+++ index.js
@@ -25,16 +25,15 @@ app.get("/api/products", async (req, res) => {
   }
 });
 
-app.get('/api/products/:id', async (req,res) => {
-  try{
-    const {id} = req.params
-    const product = await Product.findById(id)
-    res.status(200).json(product)
+app.get("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
-  catch(error) {
-    res.status(500).json({message: error.message})
-  }
-})
+});
 
 app.post("/api/products", async (req, res) => {
   try {
@@ -44,6 +43,43 @@ app.post("/api/products", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+
+app.put("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    const updateProduct = await Product.findById(id);
+    res.status(200).json(updateProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.delete('/api/products/:id', async (req,res) => {
+
+  try
+  {
+    const {id} = req.params
+    
+    const product = await Product.findByIdAndDelete(id)
+
+    if(!product) 
+      {
+        return res.status(404).json({message: 'Product not found'})
+      }
+
+      res.status(500).json({message: 'Product deleted successfully'})
+  }
+  catch (error)
+  {
+    res.status(500).json({message: error.message})
+  }
+})
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/CRUD")
