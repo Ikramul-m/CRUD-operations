0a160dbe1d5a1ff192f8f7d14569348769116fbf
Ikramul-m <i20727172@gmail.com>
2024-05-08 09:34:46 +0530

    .

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Product = require("./Models/Product.js");
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 const PORT = 3000;
 const hostName = "localhost";
@@ -15,13 +16,17 @@ app.get("/", (req, res) => {
   res.send("Hello from Node API");
 });
 
-app.post("/api/products", (req, res) => {
-  console.log(req.body);
-  res.send("Data received");
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/operations")
+  .connect("mongodb://127.0.0.1:27017/CRUD")
   .then(() => {
     console.log("Connected to Database.");
   })
